e9d87eda5bb2f5220564eee495faff9518d414f5
Dave Templin <dtempx@gmail.com>
2022-11-29 11:26:30 -0800

    remove old code

--- src/panel/Accordian.tsx
+++ src/panel/Accordian.tsx
@@ -4,33 +4,19 @@ import * as Icons from "@mui/icons-material";
 import ScriptView from "./TemplateView";
 import DataView from "./DataView";
 
-/*
-const useStyles = makeStyles((theme: any) => ({
-    root: {
-        width: "100%"
-    },
-    heading: {
-        fontSize: theme.typography.pxToRem(15),
-        //fontWeight: theme.typography.fontWeightRegular
-    }
-}));
-*/
-
 export default () => {
     const [scriptExpanded, setScriptExpanded] = useState(true);
     const [dataExpanded, setDataExpanded] = useState(true);
 
-    //const classes = useStyles();
-
     return (
-        <div /*className={classes.root}*/>
+        <div>
             <Accordion expanded={scriptExpanded} onChange={(event, isExpanded) => setScriptExpanded(isExpanded)}>
                 <AccordionSummary
                     expandIcon={<Icons.ExpandMore />}
                     aria-controls="script-content"
                     id="script"
                 >
-                    <Typography /*className={classes.heading}*/>Template XYZ</Typography>
+                    <Typography>Template XYZ</Typography>
                 </AccordionSummary>
                 <AccordionDetails>
                     <ScriptView />
@@ -42,7 +28,7 @@ export default () => {
                     aria-controls="data-content"
                     id="data"
                 >
-                    <Typography /*className={classes.heading}*/>Data</Typography>
+                    <Typography>Data</Typography>
                 </AccordionSummary>
                 <AccordionDetails>
                     <DataView />
