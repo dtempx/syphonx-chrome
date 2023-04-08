059191858ecb0972eac57ab137165155baeec822
Dave Templin <dtempx@gmail.com>
2023-04-07 18:22:55 -0700

    add deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,16 @@
+#!/bin/bash
+version=v$(sed -n 's/.*"version": "\(.*\)",/\1/p' dist/manifest.json)
+last_tag=$(git describe --tags --abbrev=0)
+if [ "$version" = "$last_tag" ]; then
+  echo "manifest.json version not updated"
+  exit 1
+fi
+[ -d "tmp" ] && rm -rf "tmp"
 mkdir tmp
-cd tmp
+pushd tmp
 cp -r ../dist syphonx
 zip -r syphonx.zip syphonx
-gh release create v0.9.7 syphonx.zip --generate-notes
+gh release create "$version" syphonx.zip \
+    --prerelease \
+    --notes-start-tag "$last_tag"
+popd
