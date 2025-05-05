d6bf70b15f9a2e241bf099a31d572c7a21900414
ps-dbeccaria <daniel.beccaria@pricespider.com>
2025-05-05 11:46:59 -0700

    Update version and use prod url

--- src/panel/TemplateEditor/Sidebar/AutorunDialog.tsx
+++ src/panel/TemplateEditor/Sidebar/AutorunDialog.tsx
@@ -28,7 +28,7 @@ import {
 
 } from "@mui/icons-material";
 
-const api = new RestApi("http://localhost:8081");
+const api = new RestApi("https://us-central1-ps-bigdata.cloudfunctions.net/syphonx-service");
 
 export interface Props {
     open: boolean;
